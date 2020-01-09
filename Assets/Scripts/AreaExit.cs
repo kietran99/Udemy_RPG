51e119d5adf4115e72907f72215546342e265ebe
kietran99 <kietran99@gmail.com>
2020-01-09 21:21:14 +0700

    add character animation and create new scene

---
+++ Assets/Scripts/AreaExit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaExit : MonoBehaviour
+{
+    public string areaToLoad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(BoxCollider2D other)
+    {
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Rigidbody2D theRB;
+
+    public float moveSpeed;
+
+    public Animator myAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        theRB.velocity = new Vector2(moveX, moveY) * moveSpeed;
+
+        myAnim.SetFloat("moveX", theRB.velocity.x);
+        myAnim.SetFloat("moveY", theRB.velocity.y);
+
+        if (moveX == 1 || moveX == -1 || moveY == 1 || moveY == -1)
+        {
+            myAnim.SetFloat("lastMoveX", moveX);
+            myAnim.SetFloat("lastMoveY", moveY);
+        }
+    }
+}
